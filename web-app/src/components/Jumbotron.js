eac22761f1ea8afa9bd282e3d7d35c68149ab5d6
mustafamerttunali <tunalim@mef.edu.tr>
2020-05-30 23:33:43 +0300

    bug fixed

--- web-app/src/components/Jumbotron.js
+++ web-app/src/components/Jumbotron.js
@@ -5,9 +5,7 @@ import Container from "react-bootstrap/Container"
 import style from "./style.css"
 function Jumbotron_Header()
 {
-    const img1 = require("../assets/slider/we_are_fusion.PNG");
-    const imgSize = "50%";
-   
+
     return(
         <Jumbotron fluid>
     <Container>
